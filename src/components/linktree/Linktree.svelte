b5da6ca1ccb3b03d8a9cd384022c60ee8f7ce023
nickmura <nickmuradev@gmail.com>
2023-01-18 16:18:58 -0700

    Changed image names

--- src/components/linktree/Linktree.svelte
+++ src/components/linktree/Linktree.svelte
@@ -9,7 +9,7 @@
 <main class='overflow-hidden'>
     <div class='flex justify-center mt-14 mb-6'>
         <img
-            src='/player4.png'
+            src='/avatar.png'
             alt="https://www.npmjs.com/package/ethereum-blockies/v/0.1.0?activeTab=readme"
             class="h-24 w-24 rounded-full object-cover sm:h-24 sm:w-24 group-hover:opacity-70 shadow-lg"
         >
@@ -18,7 +18,7 @@
     <a class='w-full' href='https://github.com/nickmura' alt=''>
         <div class="flex justify-center hover:scale-[1.05] transition transition-200">
             <div class="mt-8 ml-6 mr-6 flex w-full max-w-[680px] rounded-lg border bg-[#F9F5E3] px-3 py-3 shadow-lg relative">
-            <img src="/github_profile_pic.png" alt="https://github.com/nickmura" class="absolute h-10 w-10 rounded-full top-[50%] translate-y-[-50%]" />
+            <img src="/github.png" alt="https://github.com/nickmura" class="absolute h-10 w-10 rounded-full top-[50%] translate-y-[-50%]" />
             <div class="flex w-full justify-center text-2xl">GitHub</div>
             </div>
         </div>
